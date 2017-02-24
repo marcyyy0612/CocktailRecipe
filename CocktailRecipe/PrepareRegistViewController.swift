4e729b47d876e52008f5bea2cc675ec9f50b6158
Masashi Hamano <Marcy@MasashinoMacBook-Pro.local>
2017-02-25 00:59:34 +0900

    modify segue regist

---
+++ CocktailRecipe/PrepareRegistViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PrepareRegistViewController.swift
+//  CocktailRecipe
+//
+//  Created by Masashi Hamano on 2017/02/25.
+//  Copyright © 2017年 Marcy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PrepareRegistViewController : UIViewController {
+    
+    @IBOutlet weak var registBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
